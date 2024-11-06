ae697d6c6488a4b72720d206b06e3a0e93a2d163
elihu26 <evaldelo22@jaumebalmes.net>
2024-11-06 10:48:17 +0100

    commit con prueba de web

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -15,4 +15,21 @@
     Route::put('/pedidos/{id}', [PedidoController::class, 'update'])->name('pedidos.update');
     Route::delete('/pedidos/{id}', [PedidoController::class, 'destroy'])->name('pedidos.destroy');
 
-    Route::post('/compras', [CompraController::class, 'listaCompra'])->name('compras.store');
+    Route::get('/correo-de-prueba', function () {
+        // Datos de prueba
+        $compra = (object)[
+            'email' => '', // Correo real
+            'precio_total' => 120.00,
+            'estado' => 'recibido'
+        ];
+    
+        $productosDetalles = [
+            (object)['id_prenda' => 1, 'talla' => 'XL', 'precio' => 40.00],
+            (object)['id_prenda' => 2, 'talla' => 'XXL', 'precio' => 80.00]
+        ];
+    
+        // Enviar el correo
+        Mail::to($compra->email)->send(new PedidoRealizado($compra, $productosDetalles));
+    
+        return 'Correo de prueba enviado a la compañera!';
+    });
