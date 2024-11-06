d0ab37e8f36aae04fad8c77230a9a6737991ffb3
elihu26 <evaldelo22@jaumebalmes.net>
2024-11-06 11:44:20 +0100

    prueba 2 emial produccion

--- backend/app/Http/Controllers/CompraController.php
+++ backend/app/Http/Controllers/CompraController.php
@@ -4,7 +4,9 @@
     use Illuminate\Http\Request;
     use App\Models\Compra;
     use App\Models\DetalleCompra;
-
+    use Illuminate\Support\Facades\Mail;
+    use App\Mail\PedidoRealizado;
+    
     class CompraController extends Controller {
         public function listaCompra(Request $request) {
             // Validación de la entrada
@@ -12,27 +14,35 @@
                 'email' => 'required|email',
                 'total' => 'required|numeric',
                 'productos' => 'required|array',
-                    'productos.*.id_prenda' => 'required|integer',
-                    'productos.*.talla' => 'required|string',
-                    'productos.*.precio' => 'required|string'
+                'productos.*.id_prenda' => 'required|integer',
+                'productos.*.talla' => 'required|string',
+                'productos.*.precio' => 'required|numeric'  // Cambiado a 'numeric'
             ]);
-
+    
             // Crear la entrada principal en la tabla 'compras'
             $compra = Compra::create([
                 'email' => $request->email,
                 'precio_total' => $request->total,
+                'estado' => 'recibido',  // Añadido estado por defecto
             ]);
-
+    
             // Crear detalles de compra
             foreach ($request->productos as $producto) {
                 DetalleCompra::create([
                     'id_compra' => $compra->id_compra,
                     'id_prenda' => $producto['id_prenda'],
                     'talla' => $producto['talla'],
+                    'precio' => $producto['precio'],  
                 ]);
             }
-            $productosDetalles = $request->productos;
-            Mail::to($compra->email)->send(new PedidoRealizado($compra, $productosDetalles));
-            return response()->json(['message' => 'Compra realizada con éxito', 'compra' => $compra], 201);
+    
+            Mail::to($compra->email)->send(new PedidoRealizado($compra, $request->productos));
+    
+            return response()->json([
+                'message' => 'Compra realizada con éxito',
+                'compra' => $compra,  // Retornar los detalles de la compra
+                'productos' => $request->productos  // Incluir los detalles de los productos en la respuesta
+            ], 201);
         }
     }
+    
