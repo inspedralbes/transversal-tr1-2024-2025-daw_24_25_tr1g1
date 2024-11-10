1f3ffc86bfc2cadcc8aaf23add530163f2b2767c
elihu26 <evaldelo22@jaumebalmes.net>
2024-11-10 20:58:03 +0100

    si

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,11 +5,11 @@
     use App\Http\Controllers\PedidoController;
     use App\Mail\PedidoRealizado;
 
-    Route::prefix('crood')->group(function () {
+    Route::prefix('crud')->group(function () {
         Route::resource('prendas', PrendaController::class);
     });
 
-    // CROOD de los pedidos realizados
+    // crud de los pedidos realizados
     Route::get('/pedidos', [PedidoController::class, 'index'])->name('pedidos.index');
     Route::get('/pedidos/{id}/edit', [PedidoController::class, 'edit'])->name('pedidos.edit');
     Route::put('/pedidos/{id}', [PedidoController::class, 'update'])->name('pedidos.update');
